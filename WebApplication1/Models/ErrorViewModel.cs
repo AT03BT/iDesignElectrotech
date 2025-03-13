e6dd89d43879683c90fa0f921dfe4f9e9d4ab859
mitra <mitra@GX01_CS750>
2025-03-14 06:53:00 +1100

    Add project files.

---
+++ WebApplication1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace iDesignElectrotech.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
